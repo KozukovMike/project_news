2a134f21fdabc142289d0c9aa63f18c08498ae12
Mike_kozyukov <kozukovmisa@gmail.com>
2023-03-05 13:54:25 +0300

    'add 1_1_gini_score'

---
+++ homework_1/1_1_gini_score.py
@@ -0,0 +1,19 @@
+import random
+from typing import List
+
+
+input_list = [random.randint(1, 3) for i in range(20)]
+
+
+def gini_score(items: List[int]) -> float:
+    length = len(items)
+    p = 0
+    while len(items) > 0:
+        current_length = len(items)
+        current_item = items[0]
+        items = list(filter(lambda x: x != current_item, items))
+        p += ((current_length - len(items)) / length)**2
+    return round(1 - p, 3)
+
+
+print('gini_result: ', gini_score(input_list))
