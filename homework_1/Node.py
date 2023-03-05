f1720d2201dfaf8c6828c9c6ca8a642edb6502a0
Mike <kozukovmisa@gmail.com>
2023-03-05 15:51:15 +0300

    'add children logic'

--- homework_1/Node.py
+++ homework_1/Node.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Optional
+from typing import Iterable, Optional, List
 
 
 class Node:
@@ -6,19 +6,47 @@ class Node:
     __number = 0
     all_objects = dict()
 
-    def __init__(self, values: Iterable, parent: Optional["Node"] = None):
+    def __init__(self,
+                 values: Iterable,
+                 parent: Optional["Node"] = None,
+                 children: Optional[List["Node"]] = None
+                 ):
         Node.__number += 1
         self.id = Node.__number
         self.values = values
-        self.children = None
-        self.parent = parent
+        if isinstance(children, Iterable):
+            self.children = []
+            for child in children:
+                if isinstance(child, Node):
+                    if Node.all_objects[child.id].parent is None:
+                        Node.all_objects[child.id].parent = self
+                        self.children.append(child)
+                    else:
+                        raise ValueError('this Node already has a parent')
+                else:
+                    raise ValueError('this object is not a Node')
+        elif isinstance(children, Node):
+            if Node.all_objects[children.id].parent is None:
+                self.children = []
+                Node.all_objects[children.id].parent = self
+                self.children.append(children)
+            else:
+                raise ValueError('this Node already has a parent')
+        elif children is None:
+            self.children = children
+        else:
+            raise ValueError('expected Optional[List["Node"]]')
         Node.all_objects[Node.__number] = self
-        if parent and isinstance(parent, Node):
+        if isinstance(parent, Node):
             self.parent = parent
             if not Node.all_objects[parent.id].children:
                 Node.all_objects[parent.id].children = [self]
             else:
                 Node.all_objects[parent.id].children.append(self)
+        elif parent is None:
+            self.parent = parent
+        else:
+            raise ValueError('expected Node')
 
     @staticmethod
     def get_by_id(id_):
@@ -35,6 +63,8 @@ class Node:
         return False
 
 
+v = Node([1, 1])
 a = Node([1, 2])
-b = Node([1, 2], a)
-print(a.children, a.parent, b.children, b.parent)
+c = Node([1, 3], parent=a)
+b = Node([1, 4], a)
+print(a.children, b.children, c.children, c.parent)
